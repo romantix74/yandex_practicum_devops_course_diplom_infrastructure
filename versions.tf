229e07e33e7090cf7319bb35ecd2bbcba7ddd41a
Roman Alekseev <alekseev-roman21@yandex.ru>
2024-03-21 13:21:23 +0000

    S3 for terraform state

--- versions.tf
+++ versions.tf
@@ -5,4 +5,15 @@ terraform {
       version = ">= 0.87.0"
     }
   }
+  
+  # Описание бэкенда хранения состояния
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "terraform-state-std-022-024"
+    region     = "ru-central1"
+    key        = "terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
