bf2c5778fbf31dfe3229302070e59a8c8f4bc7a2
Roman Alekseev <alekseev-roman21@yandex.ru>
2024-03-22 03:41:42 +0000

    k8s module

---
+++ terraform/modules/yc-k8s/main.tf
@@ -0,0 +1,90 @@
+resource "yandex_kubernetes_cluster" "k8s_zonal_cluster" {
+ network_id = yandex_vpc_network.k8s-network.id
+ master {
+   version = var.k8s_version
+   zonal {
+     zone      = yandex_vpc_subnet.k8s-subnet.zone
+     subnet_id = yandex_vpc_subnet.k8s-subnet.id
+   }
+   public_ip = true
+ }
+ service_account_id      = yandex_iam_service_account.kubernetes.id
+ node_service_account_id = yandex_iam_service_account.kubernetes.id
+   depends_on = [
+     yandex_resourcemanager_folder_iam_binding.editor,
+     yandex_resourcemanager_folder_iam_binding.images-puller
+   ]
+}
+
+resource "yandex_vpc_network" "k8s-network" { 
+  name = "k8s-network" 
+}
+
+resource "yandex_vpc_subnet" "k8s-subnet" {
+ name = "k8s-subnet"
+ v4_cidr_blocks = [var.v4_cidr_blocks]
+ zone           = var.cluster_zone
+ network_id     = yandex_vpc_network.k8s-network.id
+}
+
+resource "yandex_iam_service_account" "kubernetes" {
+ name        = "kubernetes"
+ description = "Service account for manage kubernetes"
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "editor" {
+ # Сервисному аккаунту назначается роль "editor".
+ folder_id = var.folder_id
+ role      = "editor"
+ members   = [
+   "serviceAccount:${yandex_iam_service_account.kubernetes.id}"
+ ]
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "images-puller" {
+ # Сервисному аккаунту назначается роль "container-registry.images.puller".
+ folder_id = var.folder_id
+ role      = "container-registry.images.puller"
+ members   = [
+   "serviceAccount:${yandex_iam_service_account.kubernetes.id}"
+ ]
+}
+
+resource "yandex_kubernetes_node_group" "k8s-node-group" {
+  cluster_id  = "${yandex_kubernetes_cluster.k8s_zonal_cluster.id}"
+  name        = "k8s-node-group"
+  version     = var.k8s_version
+
+  instance_template {
+    platform_id = var.platform_id
+
+    network_interface {
+      nat                = true
+      subnet_ids         = ["${yandex_vpc_subnet.k8s-subnet.id}"]
+    }
+
+    resources {
+      memory = var.node_memory
+      cores  = var.node_cores
+      core_fraction = var.node_core_fraction
+    }
+
+    boot_disk {
+      type = var.boot_disk_type
+      size = var.boot_disk_size
+    }
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = var.nodes_count
+    }
+  }
+
+  allocation_policy {
+    location {
+      zone = var.cluster_zone
+    }
+  }
+
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,30 +1,7 @@
 output "kube_cluster_id" {
-  description = "Kubernetes cluster ID."
-  value       = try(yandex_kubernetes_cluster.k8s_zonal_cluster.id, null)
+  value = module.std-022-024_k8s_cluster.kube_cluster_id
 }
 
-# output "kube_cluster_name" {
-#   description = "Kubernetes cluster name."
-#   value       = try(k8s_zonal_cluster.cluster_name, null)
-# }
-
-output "external_v4_address" {
-  description = "Connection string to external Kubernetes cluster."
-  value       = try(yandex_kubernetes_cluster.k8s_zonal_cluster.master[0].external_v4_address, null)
+output "external_cluster_cmd" {
+  value = module.std-022-024_k8s_cluster.external_cluster_cmd
 }
-
-# output "kube_cluster_id" {
-#   description = "Kubernetes cluster ID."
-#   value       = try(module.kube.cluster_id, null)
-# }
-
-# output "kube_cluster_name" {
-#   description = "Kubernetes cluster name."
-#   value       = try(module.kube.cluster_name, null)
-# }
-
-# output "external_cluster_cmd_str" {
-#   description = "Connection string to external Kubernetes cluster."
-#   value       = try(module.kube.external_cluster_cmd, null)
-# }
-
